6d0a395dabfaba41dbfba3950c36dfe267db8692
Matt Chang <chchwy@gmail.com>
2020-08-01 21:34:26 +1000

    gogo

--- app/src/aboutdialog.cpp
+++ app/src/aboutdialog.cpp
@@ -29,7 +29,6 @@ AboutDialog::AboutDialog(QWidget* parent) :
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
-
     setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::WindowCloseButtonHint);
 }
 
