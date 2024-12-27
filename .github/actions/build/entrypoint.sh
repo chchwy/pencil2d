0cab5fda067b1b8310273b3670328f43fa07bd0c
Matt Chang <chchwy@gmail.com>
2024-12-27 11:35:10 +0800

    test step level container

---
+++ .github/actions/build/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -l
+
+set -e
+ldd --version
+git --version
+curl --version
+
+echo 'IS_RELEASE=' $IS_RELEASE
+
+export LANG=C.UTF-8
+
+QT_BASE_DIR=/opt/qt515
+export QTDIR=$QT_BASE_DIR
+export PATH=$QT_BASE_DIR/bin:$PATH
+
+if [ $(uname -m) = "x86_64" ]; then
+  export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
+else
+  export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/i386-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
+fi
+export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
+
+qmake --version
+
+mkdir build
+qmake -o build PREFIX=/usr CONFIG-=debug_and_release CONFIG+=release CONFIG+=GIT CONFIG+=PENCIL2D_NIGHTLY VERSION=1.0.0
+cd build
+make -j4
+
+tree
+
+echo 'Building AppImage'
+sh ../.github/actions/build/create_app_image.sh
+echo 'AppImage built'
