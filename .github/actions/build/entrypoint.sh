e7bbe42d63c2ab384518379f8d0ce4bd9c19b336
Matt Chang <chchwy@gmail.com>
2024-12-28 19:12:42 +0800

    env

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -5,8 +5,13 @@ tree
 ldd --version
 git --version
 curl --version
-qmake --version
 
+export MAKEFLAGS=-j2
+# Our container image uses the non-Unicode C locale by default
+export LANG=C.UTF-8
+# Set up Qt environment variables and export them to the GitHub Actions workflow
+sh /opt/qt515/bin/qt515-env.sh
+qmake --version
 
 echo "Hello $1"
 time=$(date)
