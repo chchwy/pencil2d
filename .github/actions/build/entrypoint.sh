1d68b5b99730552916a25fc9896e78fb2a23cdd1
Matt Chang <chchwy@gmail.com>
2024-12-28 19:28:45 +0800

    copy qt env. sh

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -9,8 +9,16 @@ curl --version
 export MAKEFLAGS=-j2
 export LANG=C.UTF-8
 
-ls /opt/qt515/bin
-source /opt/qt515/bin/qt515-env.sh
+QT_BASE_DIR=/opt/qt515
+export QTDIR=$QT_BASE_DIR
+export PATH=$QT_BASE_DIR/bin:$PATH
+
+if [ $(uname -m) = "x86_64" ]; then
+  export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
+else
+  export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/i386-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
+fi
+export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
 qmake --version
 
