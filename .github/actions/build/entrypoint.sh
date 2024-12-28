92aa705d72088bc615ed9d1b959ca3e97ea4dc57
Matt Chang <chchwy@gmail.com>
2024-12-28 19:03:19 +0800

    yqq

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-
-
+set -e
 tree
 ldd --version
 git --version
 curl --version
 qmake --version
+
+
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
+
+qmake 
+make
